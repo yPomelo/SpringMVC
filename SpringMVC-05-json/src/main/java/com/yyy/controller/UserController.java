6f06b820066f10e18b943f2eecfb4afab68a7fb7
yPomelo <438599598@qq.com>
2020-10-19 19:17:55 +0800

    Json交互

---
+++ SpringMVC-05-json/src/main/java/com/yyy/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.yyy.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yyy.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+    @RequestMapping("/j1")
+    public String json1() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = new User("yyy",18,"男");
+        String s = objectMapper.writeValueAsString(user);
+        return s;
+    }
+    @RequestMapping("/j2")
+    public String json2() throws JsonProcessingException {
+
+        //创建一个jackson的对象映射器，用来解析数据
+        ObjectMapper mapper = new ObjectMapper();
+        //创建一个对象
+        User user1 = new User("秦疆1号", 3, "男");
+        User user2 = new User("秦疆2号", 3, "男");
+        User user3 = new User("秦疆3号", 3, "男");
+        User user4 = new User("秦疆4号", 3, "男");
+        List<User> list = new ArrayList<User>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+
+        //将我们的对象解析成为json格式
+        String str = mapper.writeValueAsString(list);
+        return str;
+    }
+}
